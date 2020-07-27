e80893dbb6449299d44d4daa60a8a1d4912be531
Ali Ogaili <ali@meronex.com>
2020-07-27 16:22:15 +0400

    refactor: improve the share cards experience

--- packages/preview/src/pages/_document.tsx
+++ packages/preview/src/pages/_document.tsx
@@ -16,7 +16,43 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="shortcut icon" href="/favicon.png" />
-          <meta name="theme-color" content="#e91e63" />
+          <meta name="theme-color" content="#F47847" />
+          <title>Meronex Icons — React Icons Package</title>
+          <meta name="title" content="Meronex Icons — React Icons Package" />
+          <meta
+            name="description"
+            content="A rich set of all the popular icons packaged as react components."
+          />
+
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://metatags.io/" />
+          <meta
+            property="og:title"
+            content="Meronex Icons — React Icons Package"
+          />
+          <meta
+            property="og:description"
+            content="A rich set of all the popular icons packaged as react components."
+          />
+          <meta
+            property="og:image"
+            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+          />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://metatags.io/" />
+          <meta
+            property="twitter:title"
+            content="Meronex Icons — React Icons Package"
+          />
+          <meta
+            property="twitter:description"
+            content="A rich set of all the popular icons packaged as react components."
+          />
+          <meta
+            property="twitter:image"
+            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+          />
           <meta name="title" content={BRAND_TITLE} />
           <meta name="description" content={BRAND_DESCRIPTION} />
           <meta name="keywords" content={BRAND_KEYWORDS} />
