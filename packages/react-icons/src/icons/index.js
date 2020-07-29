535c3fa62b0c86dc1b26462ae41e280e9870e74d
Ali Ogaili <ali@meronex.com>
2020-07-29 22:22:38 +0400

    feat: add Boxicons

--- packages/react-icons/src/icons/index.js
+++ packages/react-icons/src/icons/index.js
@@ -419,6 +419,19 @@ module.exports = {
       projectUrl: "https://github.com/twitter/twemoji",
       license: "MIT License",
       licenseUrl: "https://github.com/twitter/twemoji/blob/master/LICENSE"
+    },
+    {
+      id: "bi",
+      name: "Boxicons",
+      contents: [
+        {
+          files: path.resolve(__dirname, "boxicons/svg/regular/*.svg"),
+          formatter: name => `Bi${name.replace("Bx", "")}`
+        }
+      ],
+      projectUrl: "https://github.com/atisawd/boxicons",
+      license: "CC BY 4.0 License",
+      licenseUrl: "https://creativecommons.org/licenses/by/4.0/"
     }
   ]
 };
