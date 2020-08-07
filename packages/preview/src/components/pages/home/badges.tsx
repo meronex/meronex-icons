8412306ee3ef85ad8da3a0fe55181b3a2b7b307b
Ali Ogaili <ali@meronex.com>
2020-08-08 03:42:24 +0400

    chore: change badge reference

--- packages/preview/src/components/pages/home/badges.tsx
+++ packages/preview/src/components/pages/home/badges.tsx
@@ -3,15 +3,8 @@ import React from "react";
 function Badges() {
   return (
     <p>
-      <a href="https://www.npmjs.com/package/react-icons" rel="nofollow">
-        <img src="https://img.shields.io/npm/v/react-icons.svg" alt="npm" />
-      </a>
-      &emsp;
-      <a href="https://travis-ci.com/react-icons/react-icons" rel="nofollow">
-        <img
-          src="https://travis-ci.com/react-icons/react-icons.svg?branch=master"
-          alt="travis ci"
-        />
+      <a href="https://www.npmjs.com/package/@meronex/icons" rel="nofollow">
+        <img src="https://img.shields.io/npm/v/@meronex/icons.svg" alt="npm" />
       </a>
     </p>
   );
