99d63ffe620c9af6768b0a58c6bb679ac0c729de
Ali Ogaili <ali@meronex.com>
2020-08-07 22:02:52 +0400

    chore: a bit of styling to the preview site

--- packages/preview/src/pages/index.tsx
+++ packages/preview/src/pages/index.tsx
@@ -5,7 +5,7 @@ import ReactMarkdown from "react-markdown";
 
 import LgGithubOctocat from "@meronex/icons/lg/LgGithubOctocat";
 import LgNpm from "@meronex/icons/lg/LgNpm";
-import { BRAND_TITLE, HOME_USAGE } from "@utils/constants";
+import { BRAND_TITLE, HOME_USAGE_DEFAULT, HOME_USAGE_NAMED } from "@utils/constants";
 
 import React from "react";
 
@@ -20,31 +20,43 @@ export default function HomePage() {
   };
   return (
     <Container title={BRAND_TITLE}>
-      <Badges />
-      <p>
-        Choose from thousands of popular open source SVG icon packs and add them
-        easily to your React projects with meronex icons.
-      </p>
-      <h1>Installation</h1>
-      <CodeBlock code={`npm install @meronex/icons --save`} />
-      <h1>Usage</h1>
-      <CodeBlock code={HOME_USAGE} />
-      <ReactMarkdown source={ROADMAP} />
-      <ReactMarkdown source={CHANGELOG} />
-      <h2>More info</h2>
-      <p>
-        <a href="https://github.com/aliogaili/meronex-icons" target={"_blank"}>
-          <LgGithubOctocat style={styleIcon} /> &nbsp; Github Repo &#8599;
-        </a>
-      </p>
-      <p>
-        <a
-          href="https://www.npmjs.com/package/@meronex/icons"
-          target={"_blank"}
-        >
-          <LgNpm style={styleIcon} /> &nbsp; NPM Package &#8599;
-        </a>
-      </p>
+      <div style={{
+        lineHeight: '28px'
+      }}>
+        <Badges />
+        <p>
+          Choose from thousands of popular open source SVG icon packs and add
+          them easily to your React projects with meronex icons.
+        </p>
+        <h1>Installation</h1>
+        <CodeBlock code={`npm install @meronex/icons --save`} />
+        <h1>Usage</h1>
+
+        <h3>Default Import</h3>
+        <CodeBlock code={HOME_USAGE_DEFAULT} />
+        <h3>Named Import </h3>
+        Use named imports only when you build supports tree shaking your generated your bundle size.
+        <CodeBlock code={HOME_USAGE_NAMED} />
+        <ReactMarkdown source={ROADMAP} />
+        <ReactMarkdown source={CHANGELOG} />
+        <h2>More info</h2>
+        <p>
+          <a
+            href="https://github.com/aliogaili/meronex-icons"
+            target={"_blank"}
+          >
+            <LgGithubOctocat style={styleIcon} /> &nbsp; Github Repo &#8599;
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://www.npmjs.com/package/@meronex/icons"
+            target={"_blank"}
+          >
+            <LgNpm style={styleIcon} /> &nbsp; NPM Package &#8599;
+          </a>
+        </p>
+      </div>
     </Container>
   );
 }
--- packages/preview/src/utils/constants.ts
+++ packages/preview/src/utils/constants.ts
@@ -4,8 +4,17 @@ export const BRAND_DESCRIPTION =
   "Include popular icons in your React projects easily with meronex icons.";
 export const BRAND_KEYWORDS = "React, Icons, Tree Shaking, SVG";
 
-export const HOME_USAGE = `
+export const HOME_USAGE_DEFAULT = `
 import FaBeer from '@meronex/icons/fa/FaBeer';
+
+class Question extends React.Component {
+  render() {
+    return <h3> Lets go for a <FaBeer />? </h3>
+  }
+}`;
+
+export const HOME_USAGE_NAMED = `
+import { FaBeer } from '@meronex/icons/fa';
 
 class Question extends React.Component {
   render() {
